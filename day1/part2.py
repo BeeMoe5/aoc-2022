f6981822f8a9c13d92a6b24514a5fa70fdac1874
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-12-03 22:14:42 -0500

    actually completed day 1
    there was a part 2 lol

---
+++ day1/part2.py
@@ -0,0 +1,10 @@
+def main():
+	with open("input.txt", 'r') as file:
+		inputs = file.read().split("\n\n")
+		inputs = list(map(lambda food: sum([int(f) for f in food.splitlines()]), inputs))
+		inputs.sort(reverse=True)
+		print(sum((inputs[0], inputs[1], inputs[2])))
+
+
+if __name__ == "__main__":
+	main()
