da6a095456199e9946c6d500f2d9bb665518f764
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-12-03 21:53:18 -0500

    day 1 completed

---
+++ day1/main.py
@@ -0,0 +1,17 @@
+def main():
+	with open("input.txt", 'r') as file:
+		inputs = file.read().split("\n\n")
+		inputs = map(lambda food: sum([int(f) for f in food.splitlines()]), inputs)
+
+	largest = None
+	for inp in inputs:
+		if largest:
+			largest = inp if inp > largest else largest
+		else:
+			largest = inp
+
+	print(largest)
+
+
+if __name__ == "__main__":
+	main()
