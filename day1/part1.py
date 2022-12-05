026d836fa87b4a1f28c46d30f8a3dff6c4710ee7
BeeMoe5 <48176165+BeeMoe5@users.noreply.github.com>
2022-12-05 01:42:07 -0500

    Added and ran black

--- day1/part1.py
+++ day1/part1.py
@@ -1,17 +1,17 @@
 def main():
-	with open("input.txt", 'r') as file:
-		inputs = file.read().split("\n\n")
-		inputs = map(lambda food: sum([int(f) for f in food.splitlines()]), inputs)
+    with open("input.txt", "r") as file:
+        inputs = file.read().split("\n\n")
+        inputs = map(lambda food: sum([int(f) for f in food.splitlines()]), inputs)
 
-	largest = None
-	for inp in inputs:
-		if largest:
-			largest = inp if inp > largest else largest
-		else:
-			largest = inp
+    largest = None
+    for inp in inputs:
+        if largest:
+            largest = inp if inp > largest else largest
+        else:
+            largest = inp
 
-	print(largest)
+    print(largest)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- day1/part2.py
+++ day1/part2.py
@@ -1,10 +1,12 @@
 def main():
-	with open("input.txt", 'r') as file:
-		inputs = file.read().split("\n\n")
-		inputs = list(map(lambda food: sum([int(f) for f in food.splitlines()]), inputs))
-		inputs.sort(reverse=True)
-		print(sum((inputs[0], inputs[1], inputs[2])))
+    with open("input.txt", "r") as file:
+        inputs = file.read().split("\n\n")
+        inputs = list(
+            map(lambda food: sum([int(f) for f in food.splitlines()]), inputs)
+        )
+        inputs.sort(reverse=True)
+        print(sum((inputs[0], inputs[1], inputs[2])))
 
 
 if __name__ == "__main__":
-	main()
+    main()
